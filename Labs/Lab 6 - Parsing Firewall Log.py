b24af89fa7bd7d98349a657d3ea1e979acabfb02
Maks Derevencha <maks.derevencha@MacBook-Pro.local>
2024-08-04 21:26:39 -0400

    more logs in lab 6

--- Labs/Lab 6 - Parsing Firewall Log.py
+++ Labs/Lab 6 - Parsing Firewall Log.py
@@ -11,20 +11,22 @@ bonusStep = 0
 
 #2024-01-29 14:50:00,10.0.0.9,INTERNAL,ALLOW,inbound,web4.example.com
 with open('Labs/Lab 6 - Log File.txt', 'r') as file:
-    for line in file:
-        fields = line.strip().split(',')
-        if len(fields) == 6:
-            timestamp, source_ip, zone, action, direction, destination = fields
-            if action == "DENY" and "ssh" in destination:
-                bonusStep +=1
-            print("Timestamp:", timestamp)
-            print("Source IP:", source_ip)
-            print("Zone:", zone)
-            print("Action:", action)
-            print("Direction:", direction)
-            print("Destination:", destination)
-            print()
-        else:
-            print("Invalid log entry:", line)
+    
+        for line in file:
+            fields = line.strip().split(',')
+            if len(fields) == 6:
+                timestamp, source_ip, zone, action, direction, destination = fields
+                if action == "DENY" and "ssh" in destination:
+                    bonusStep +=1
+                print("Timestamp:", timestamp)
+                print("Source IP:", source_ip)
+                print("Zone:", zone)
+                print("Action:", action)
+                print("Direction:", direction)
+                print("Destination:", destination)
+                print()
+            else:
+                print("Invalid log entry:", line)
+
 
 print(f"There were {bonusStep} logs entries that were SSH destination and Blocked\n")
